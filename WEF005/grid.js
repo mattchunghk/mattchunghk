71f664f303ee085ad6bfe1e575cacfbee342f674
Matt Chung <mattchunghk@gmail.com>
2022-08-16 10:30:33 +0800

    add

--- WEF005/grid.js
+++ WEF005/grid.js
@@ -17,3 +17,59 @@ function slider() {
     dots[slideIndex - 1].style.backgroundColor = "#443a1c"
     setTimeout(slider, 4000)
 }
+
+// let audio = new Audio("https://www.cssigniter.com/assets/audio/new_track_05.mp3");
+
+
+// document.querySelector('#play-button').addEventListener("canplaythrough", function(event) {
+//     audio.play();
+
+// });
+
+
+let audios = document.querySelectorAll("#myAudio");
+let plays = document.querySelectorAll("#play-button")
+let pauses = document.querySelectorAll("#pause-button")
+
+function playMusic() {
+    console.log(plays)
+
+    for (let i = 0; i < plays.length; i++) {
+        plays[i].addEventListener("click", function(event) {
+            audios[i].play()
+            plays[i].style.display = "none";
+            pauses[i].style.display = "block";
+
+        })
+    }
+}
+
+playMusic()
+
+
+function pauseMusic() {
+    for (let i = 0; i < pauses.length; i++) {
+        pauses[i].addEventListener("click", function(event) {
+            audios[i].pause()
+            plays[i].style.display = "block";
+            pauses[i].style.display = "none";
+        })
+    }
+}
+
+pauseMusic()
+
+
+
+
+let x = document.querySelector(".bi-x-square");
+let ow = document.querySelector(".overview")
+let gm = document.querySelector("#glasses_man");
+
+x.addEventListener("click", function(event) {
+    ow.style.visibility = "hidden";
+})
+
+gm.addEventListener("click", function(event) {
+    ow.style.visibility = "visible";
+})
